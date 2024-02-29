69cfce754568c486eb63534f0c5dfaacd0411c4b
Austin Slater <austinslater58@gmail.com>
2024-02-29 13:16:41 -0600

    additonal task columns added by user saves heading and renders a container for the heading if it exists

--- MyMvcApp/wwwroot/js/site.js
+++ MyMvcApp/wwwroot/js/site.js
@@ -1,6 +1,8 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+
+
 // Check if localStorage is supported by the browser
 if (typeof (Storage) !== "undefined") {
   // Load saved column names from localStorage when the page loads
@@ -11,13 +13,22 @@ if (typeof (Storage) !== "undefined") {
   console.log("Sorry, your browser does not support web storage...");
 }
 
-// Adds columns to kanban board
 function addColumn() {
-  var kanbanBoard = document.getElementById('kanbanBoard');
+  var dynamicColumns = document.getElementById('dynamicColumns');
   var newColumn = document.createElement('div');
+
+  // Generate unique id for the new column
+  var newColumnId = "column" + (dynamicColumns.children.length + 4); // Start from 4 to avoid conflicting with initial columns
+
   newColumn.className = 'col border';
-  newColumn.innerHTML = '<h3 contenteditable="true">New Column:</h3><button class="bg-secondary text-light rounded">+</button><button class="bg-danger text-light rounded" onclick="removeColumn(this)">x</button>';
-  kanbanBoard.appendChild(newColumn);
+  newColumn.innerHTML = '<h3 contenteditable="true" onblur="saveColumn(this)" id="' + newColumnId + '">New Column:</h3><button class="bg-secondary text-light rounded">+</button><button class="bg-danger text-light rounded" onclick="removeColumn(this)">x</button>';
+  dynamicColumns.appendChild(newColumn);
+
+  // Log the new column content
+  console.log(newColumn);
+
+  // Call saveColumn() immediately after adding the new column
+  saveColumn(newColumn.querySelector('h3'));
 }
 
 
@@ -25,25 +36,39 @@ function addColumn() {
 function removeColumn(button) {
   let confirmed = confirm("Are you sure you want to delete?");
   if (confirmed) {
+    // Get the id of the column being removed
+    var columnId = button.parentNode.querySelector('h3').id;
+
     // Removes the parent node (column div) from the DOM
     button.parentNode.remove();
-    // Save columns after removal
-    saveColumn();
+
+    // Remove the corresponding data from local storage
+    localStorage.removeItem(columnId);
   }
 }
 
-// Save column name in localStorage
 function saveColumn(element) {
-  var columnName = element.innerText;
-  localStorage.setItem(element.id, columnName);
+  var columnId = element.id;
+  
+  // Save the entire HTML content of the column
+  var columnData = element.parentElement.outerHTML; 
+  localStorage.setItem(columnId, columnData);
 }
 
+
+
+
+
 function loadColumns() {
-  var columns = document.querySelectorAll("#kanbanBoard .col h3");
-  columns.forEach(function (column) {
-    var columnName = localStorage.getItem(column.id);
-    if (columnName) {
-      column.innerText = columnName;
+  var dynamicColumns = document.getElementById("dynamicColumns");
+
+  for (var i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i);
+    if (key.startsWith("column")) {
+      var columnData = localStorage.getItem(key);
+      var columnElement = document.createElement('div');
+      columnElement.innerHTML = columnData;
+      dynamicColumns.appendChild(columnElement);
     }
-  });
+  }
 }
